ec78c10352defe590db89831b978aa8f28a50f55
loryleticee <loryleticee@gmail.com>
2021-10-28 17:15:05 +0200

    Insertion en base donnée OK

--- formulaire.php
+++ formulaire.php
@@ -1,7 +1,11 @@
 <?php
+require_once 'config.php';
 
-$username = $_POST["username"] ;
-$password = $_POST["password"] ;
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
 
-echo $username . " " . $password;
+$stmt = $pdo->prepare("INSERT INTO `users` (`username` ,`password`, `email`) VALUES (:username, :mdp, :email)");
+$stmt->execute(['username' => $username , 'mdp' => $password, ':email' => $email,]);
+
 
