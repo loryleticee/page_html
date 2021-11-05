fa59dab90833a1528cd30bb4bf867595a3cc1cea
loryleticee <loryleticee@gmail.com>
2021-11-05 17:09:06 +0100

    grow fontawesome

--- js/api.js
+++ js/api.js
@@ -13,6 +13,22 @@ function showApi() {
       });
 }
 
+function showWeather() {
+    let city = document.getElementById("cities").value;
+    let token = "25559ba9549cdf970b482a35d3313cac";
+    let url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+token;
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+            showDatas(response, city);
+        },
+        error: function (err) {
+            console.log("error", err)
+        }
+      });
+}
 
 function getCities() {
     let url = "https://countriesnow.space/api/v0.1/countries/cities";
@@ -35,3 +51,38 @@ function getCities() {
         }
       });
 }
+
+function showDatas(datas, city) {
+    console.log("city", city)
+    // console.log("OBJECT: ", datas.weather[0])
+
+    let object_to_array = Object.entries(datas.weather[0]);
+    console.log("ARRAY: ", object_to_array)
+
+    //Element dans lequel j'affiche les resultats
+    let target = document.getElementById("container");
+
+    let key_selected = ["clearsky", "brokenclouds", "fewclouds", "overcastclouds"];
+
+    let assoc_array =  {
+        "brokenclouds" : "<i class='fas fa-cloud-sun fa-10x'></i>",
+        "fewclouds" : "<i class='fas fa-cloud-rain fa-10x'></i>",
+        "clearsky" : "<i class='fas fa-sun fa-10x'></i>",
+        "overcastclouds" : "<i class='fas fa-cloud-showers-heavy fa-10x'></i>",
+    }
+    
+    for( let [key, value] of object_to_array ) {
+        value = String(value).toLowerCase().replace(' ', '');
+        if( key_selected.includes(value) ) {
+            console.log("value", value)
+            console.log("TTT",assoc_array[value])
+            let new_paragraph = document.createElement('div');
+            new_paragraph.setAttribute('style', "display:flex;justify-content:center;align-content:center;")
+            new_paragraph.setAttribute('class', 'main-weather')
+            new_paragraph.innerHTML =  "<span>" + city.toUpperCase() + " " + assoc_array[value] + "</span>";
+            target.appendChild(new_paragraph);
+        }
+    }
+
+}
+
