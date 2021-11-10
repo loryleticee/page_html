d3f65d86f685968043ec459d8b17739155950ad2
loryleticee <loryleticee@gmail.com>
2021-11-10 08:41:11 +0100

    add comment for VUE integration

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="keywords" content="chaussure, e-commerce, vente, chausette">
     <meta name="author" content="Lory LÉTICÉE">
     <link rel="stylesheet" href="./style.css">
+    <!-- ADD VUE CDN HERE -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <title>
